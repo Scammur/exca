7753e11901212cc5a7640ae432cc913332ff5fc4
0ct0 <38650186+Scammur@users.noreply.github.com>
2023-10-21 10:39:50 +0800

    Add files via upload

--- up.py
+++ up.py
@@ -6,8 +6,7 @@ from sys import platform as pf
 sm('clear')
 print("\033[1;32nCHECKING UPDATE")
 sp(5)
-if "Already up to date." in str(sm('git pull')):
-    print("UPDATED")
-else:
-    print("New Version Found")
-    sm('git pull')
+sm("git pull")
+sm('clear')
+print("\033[1;32mUPDATE DONE")
+print("\033[1;36mNow Run python excaa.py")
